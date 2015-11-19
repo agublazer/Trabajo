9c7e54e8ee7f1557facac3d9fc3859710f74b294
Valentino <dany.diaz@ucsp.edu.pe>
2015-11-19 08:03:39 -0500

    Update stack.h

--- Tarea 2/stack.h
+++ Tarea 2/stack.h
@@ -57,10 +57,7 @@ public:
         for(int i=0;i<s_3.max_size;i++)
         {
 		if (stack_items[i]==s_2.stack_items[i])
-            		s_3.stack_items[i]=stack_items[i];
-            		
-		if (stack_items[i]!=s_2.stack_items[i])
-			s_3.stack_items=0;
+            		s_3.stack_items[i]=0;
         }
 	return s_3;
 };
