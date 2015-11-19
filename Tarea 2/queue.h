8e929a06d0413be72e156872c45ac63fa7d20e91
Valentino <dany.diaz@ucsp.edu.pe>
2015-11-19 07:57:58 -0500

    Update queue.h

--- Tarea 2/queue.h
+++ Tarea 2/queue.h
@@ -27,28 +27,28 @@ public:
         }
         return q_3;
     }
-	Queue<T> operator -(Queue<T> q_2)
+    //resta:
+    Queue<T> operator -(Queue<T> q_2)
     {
         Queue<T> q_3;
         if(size>=q_2.size)
-		{
-		    q_3.size=size;
-		}
+	{
+		q_3.size=size;
+	}
 
-		if(size<q_2.size)
-		{
-		    q_3.size=q_2.size;
-		}
+	if(size<q_2.size)
+	{
+		q_3.size=q_2.size;
+	}
 			
 
         for(int i=0;i<q_3.size;i++)
         {
-		    if (items[i]==q_2.items[i])
-                q_3.items[i]=items[i];
-
-			if (items[i]!=q_2.items[i])
-				q_3.items=0;
-        }
-		  return q_3;
+		if (items[i]==q_2.items[i])
+                	q_3.items[i]=items[i];
+                	
+		if (items[i]!=q_2.items[i])
+			q_3.items=0;
 	}
+	return q_3;
 };
